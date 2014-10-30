27163a93e6b91a6b739ef18eb8b54fa77affae3c
dbenson24 <derek.benson@tufts.edu>
2014-10-30 20:28:41 +0000

    Finally got php to work!

---
+++ footer.php
@@ -0,0 +1,21 @@
+<?php
+echo "<footer>
+    <div class='container footer'>
+      <p>&copy; Derek Benson</p>
+    </div>
+  </footer>
+
+  <!-- /container -->
+
+  <!-- Bootstrap core JavaScript
+    ================================================== -->
+  <!-- Placed at the end of the document so the pages load faster -->
+  <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>
+  <script src='./bootstrap/js/bootstrap.min.js'></script>
+  <script src='./bootstrap/js/smoothscroll.js'></script>
+  <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+  <script src='./js/ie10-viewport-bug-workaround.js'></script>
+</body>
+
+</html>";
+?>
--- header.php
+++ header.php
@@ -1,24 +1,57 @@
 <?php
-echo "<div class='navbar navbar-inverse navbar-fixed-top' role='navigation'>
-  <div class='container'>
-    <div class='navbar-header'>
-      <button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='.navbar-collapse'>
-        <span class='sr-only'>Toggle navigation</span>
-        <span class='icon-bar'></span>
-        <span class='icon-bar'></span>
-        <span class='icon-bar'></span>
-      </button>
-      <a class='navbar-brand' href='#jumbo'>One Million Lines_
-            </a>
-    </div>
-    <div class='collapse navbar-collapse'>
-      <ul class='nav navbar-nav navbar-right'>
-        <li><a href='#who-we-are'>WHO WE ARE</a>
-        </li>
-        <li><a href='#get-involved'>GET INVOLVED</a>
-        </li>
-      </ul>
+echo "<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+  <meta charset='utf-8'>
+  <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+  <meta name='viewport' content='width=device-width, initial-scale=1'>
+  <meta name='description' content=''>
+  <meta name='author' content=''>
+  <link rel='icon' href='../../favicon.ico'>
+
+  <title>Jumbotron Template for Bootstrap</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href='./bootstrap/css/bootstrap.min.css' rel='stylesheet'>
+  <link rel='stylesheet' href='./bootstrap/fonts/font-awesome/css/font-awesome.min.css'>
+  <link href='//fonts.googleapis.com/css?family=Arvo' rel='stylesheet' type='text/css'>
+
+  <!-- Custom styles for this template -->
+  <link href='custom.css' rel='stylesheet'>
+
+  <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+      <script src='https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js'></script>
+      <script src='https://oss.maxcdn.com/respond/1.4.2/respond.min.js'></script>
+    <![endif]-->
+
+  <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>
+
+</head>
+
+<body data-spy='scroll' data-target='.navbar-collapse' data-offset='50'>
+  <div class='navbar navbar-inverse navbar-fixed-top' role='navigation'>
+    <div class='container'>
+      <div class='navbar-header'>
+        <button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='.navbar-collapse'>
+          <span class='sr-only'>Toggle navigation</span>
+          <span class='icon-bar'></span>
+          <span class='icon-bar'></span>
+          <span class='icon-bar'></span>
+        </button>
+        <a class='navbar-brand' href='#jumbo'>One Million Lines_
+          </a>
+      </div>
+      <div class='collapse navbar-collapse'>
+        <ul class='nav navbar-nav navbar-right'>
+          <li><a href='#who-we-are'>WHO WE ARE</a>
+          </li>
+          <li><a href='#get-involved'>GET INVOLVED</a>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
-</div>";
+";
 ?>
