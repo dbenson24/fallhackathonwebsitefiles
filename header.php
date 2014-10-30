ae0de7c535bbb3a739ae1a9d01349d6b29cb7104
dbenson24 <derek.benson@tufts.edu>
2014-10-30 16:18:57 +0000

    Trying a php include using echo

---
+++ header.php
@@ -0,0 +1,24 @@
+<?php
+echo "<div class='navbar navbar-inverse navbar-fixed-top' role='navigation'>
+  <div class='container'>
+    <div class='navbar-header'>
+      <button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='.navbar-collapse'>
+        <span class='sr-only'>Toggle navigation</span>
+        <span class='icon-bar'></span>
+        <span class='icon-bar'></span>
+        <span class='icon-bar'></span>
+      </button>
+      <a class='navbar-brand' href='#jumbo'>One Million Lines_
+            </a>
+    </div>
+    <div class='collapse navbar-collapse'>
+      <ul class='nav navbar-nav navbar-right'>
+        <li><a href='#who-we-are'>WHO WE ARE</a>
+        </li>
+        <li><a href='#get-involved'>GET INVOLVED</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>";
+?>
